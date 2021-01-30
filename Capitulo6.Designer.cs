95bd6524dfd4493b6dd916e47bd3b11548f66c84
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-01-30 11:49:50 -0400

    Add project files.

---
+++ Capitulo6.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace Unidad5
+{
+    partial class Capitulo6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Capitulo6));
+            this.labcantsalones = new System.Windows.Forms.Label();
+            this.textcantsalones = new System.Windows.Forms.TextBox();
+            this.Aceptar = new System.Windows.Forms.Button();
+            this.labcantalumnos = new System.Windows.Forms.Label();
+            this.textcantalumnos = new System.Windows.Forms.TextBox();
+            this.Aceptar1 = new System.Windows.Forms.Button();
+            this.labcalifalumnos = new System.Windows.Forms.Label();
+            this.textcalifalumnos = new System.Windows.Forms.TextBox();
+            this.Aceptar2 = new System.Windows.Forms.Button();
+            this.listprom = new System.Windows.Forms.ListBox();
+            this.labprom = new System.Windows.Forms.Label();
+            this.salonsigte = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.labcalifmenor = new System.Windows.Forms.Label();
+            this.textcalifmenor = new System.Windows.Forms.TextBox();
+            this.labcalifmayor = new System.Windows.Forms.Label();
+            this.textcalifmayor = new System.Windows.Forms.TextBox();
+            this.labpromgeneral = new System.Windows.Forms.Label();
+            this.textpromgeneral = new System.Windows.Forms.TextBox();
+            this.Continuar = new System.Windows.Forms.Button();
+            this.textcantprom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labcantsalones
+            // 
+            this.labcantsalones.AutoSize = true;
+            this.labcantsalones.Location = new System.Drawing.Point(12, 45);
+            this.labcantsalones.Name = "labcantsalones";
+            this.labcantsalones.Size = new System.Drawing.Size(113, 15);
+            this.labcantsalones.TabIndex = 0;
+            this.labcantsalones.Text = "Cantidad de salones";
+            // 
+            // textcantsalones
+            // 
+            this.textcantsalones.Location = new System.Drawing.Point(215, 35);
+            this.textcantsalones.Name = "textcantsalones";
+            this.textcantsalones.Size = new System.Drawing.Size(103, 23);
+            this.textcantsalones.TabIndex = 1;
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.Location = new System.Drawing.Point(324, 35);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(75, 24);
+            this.Aceptar.TabIndex = 2;
+            this.Aceptar.Text = "Aceptar";
+            this.Aceptar.UseVisualStyleBackColor = true;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // labcantalumnos
+            // 
+            this.labcantalumnos.AutoSize = true;
+            this.labcantalumnos.Location = new System.Drawing.Point(12, 83);
+            this.labcantalumnos.Name = "labcantalumnos";
+            this.labcantalumnos.Size = new System.Drawing.Size(179, 15);
+            this.labcantalumnos.TabIndex = 3;
+            this.labcantalumnos.Text = "Cantidad de alumnos del salon 1";
+            this.labcantalumnos.Visible = false;
+            // 
+            // textcantalumnos
+            // 
+            this.textcantalumnos.Location = new System.Drawing.Point(215, 76);
+            this.textcantalumnos.Name = "textcantalumnos";
+            this.textcantalumnos.Size = new System.Drawing.Size(103, 23);
+            this.textcantalumnos.TabIndex = 4;
+            this.textcantalumnos.Visible = false;
+            // 
+            // Aceptar1
+            // 
+            this.Aceptar1.Location = new System.Drawing.Point(324, 75);
+            this.Aceptar1.Name = "Aceptar1";
+            this.Aceptar1.Size = new System.Drawing.Size(75, 24);
+            this.Aceptar1.TabIndex = 5;
+            this.Aceptar1.Text = "Aceptar";
+            this.Aceptar1.UseVisualStyleBackColor = true;
+            this.Aceptar1.Visible = false;
+            this.Aceptar1.Click += new System.EventHandler(this.Aceptar1_Click);
+            // 
+            // labcalifalumnos
+            // 
+            this.labcalifalumnos.AutoSize = true;
+            this.labcalifalumnos.Location = new System.Drawing.Point(12, 127);
+            this.labcalifalumnos.Name = "labcalifalumnos";
+            this.labcalifalumnos.Size = new System.Drawing.Size(197, 15);
+            this.labcalifalumnos.TabIndex = 6;
+            this.labcalifalumnos.Text = "Calificacion de alumno 1 del salon 1";
+            this.labcalifalumnos.Visible = false;
+            // 
+            // textcalifalumnos
+            // 
+            this.textcalifalumnos.Location = new System.Drawing.Point(215, 119);
+            this.textcalifalumnos.Name = "textcalifalumnos";
+            this.textcalifalumnos.Size = new System.Drawing.Size(103, 23);
+            this.textcalifalumnos.TabIndex = 7;
+            this.textcalifalumnos.Visible = false;
+            // 
+            // Aceptar2
+            // 
+            this.Aceptar2.Location = new System.Drawing.Point(324, 119);
+            this.Aceptar2.Name = "Aceptar2";
+            this.Aceptar2.Size = new System.Drawing.Size(75, 23);
+            this.Aceptar2.TabIndex = 8;
+            this.Aceptar2.Text = "Aceptar";
+            this.Aceptar2.UseVisualStyleBackColor = true;
+            this.Aceptar2.Visible = false;
+            this.Aceptar2.Click += new System.EventHandler(this.Aceptar2_Click);
+            // 
+            // listprom
+            // 
+            this.listprom.FormattingEnabled = true;
+            this.listprom.ItemHeight = 15;
+            this.listprom.Location = new System.Drawing.Point(12, 196);
+            this.listprom.Name = "listprom";
+            this.listprom.Size = new System.Drawing.Size(425, 139);
+            this.listprom.TabIndex = 9;
+            this.listprom.Visible = false;
+            // 
+            // labprom
+            // 
+            this.labprom.AutoSize = true;
+            this.labprom.Location = new System.Drawing.Point(12, 178);
+            this.labprom.Name = "labprom";
+            this.labprom.Size = new System.Drawing.Size(214, 15);
+            this.labprom.TabIndex = 10;
+            this.labprom.Text = "Promedio de calificaciones del salon 1: ";
+            this.labprom.Visible = false;
+            // 
+            // salonsigte
+            // 
+            this.salonsigte.Location = new System.Drawing.Point(12, 483);
+            this.salonsigte.Name = "salonsigte";
+            this.salonsigte.Size = new System.Drawing.Size(101, 32);
+            this.salonsigte.TabIndex = 12;
+            this.salonsigte.Text = "Salon siguiente";
+            this.salonsigte.UseVisualStyleBackColor = true;
+            this.salonsigte.Visible = false;
+            this.salonsigte.Click += new System.EventHandler(this.salonsigte_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(226, 483);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(101, 32);
+            this.Salir.TabIndex = 13;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Visible = false;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // labcalifmenor
+            // 
+            this.labcalifmenor.AutoSize = true;
+            this.labcalifmenor.Location = new System.Drawing.Point(12, 353);
+            this.labcalifmenor.Name = "labcalifmenor";
+            this.labcalifmenor.Size = new System.Drawing.Size(107, 15);
+            this.labcalifmenor.TabIndex = 14;
+            this.labcalifmenor.Text = "Calificacion menor";
+            this.labcalifmenor.Visible = false;
+            // 
+            // textcalifmenor
+            // 
+            this.textcalifmenor.Location = new System.Drawing.Point(191, 345);
+            this.textcalifmenor.Name = "textcalifmenor";
+            this.textcalifmenor.ReadOnly = true;
+            this.textcalifmenor.Size = new System.Drawing.Size(103, 23);
+            this.textcalifmenor.TabIndex = 15;
+            this.textcalifmenor.Visible = false;
+            this.textcalifmenor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labcalifmayor
+            // 
+            this.labcalifmayor.AutoSize = true;
+            this.labcalifmayor.Location = new System.Drawing.Point(12, 397);
+            this.labcalifmayor.Name = "labcalifmayor";
+            this.labcalifmayor.Size = new System.Drawing.Size(106, 15);
+            this.labcalifmayor.TabIndex = 16;
+            this.labcalifmayor.Text = "Calificacion mayor";
+            this.labcalifmayor.Visible = false;
+            // 
+            // textcalifmayor
+            // 
+            this.textcalifmayor.Location = new System.Drawing.Point(191, 389);
+            this.textcalifmayor.Name = "textcalifmayor";
+            this.textcalifmayor.ReadOnly = true;
+            this.textcalifmayor.Size = new System.Drawing.Size(103, 23);
+            this.textcalifmayor.TabIndex = 17;
+            this.textcalifmayor.Visible = false;
+            // 
+            // labpromgeneral
+            // 
+            this.labpromgeneral.AutoSize = true;
+            this.labpromgeneral.Location = new System.Drawing.Point(12, 443);
+            this.labpromgeneral.Name = "labpromgeneral";
+            this.labpromgeneral.Size = new System.Drawing.Size(101, 15);
+            this.labpromgeneral.TabIndex = 18;
+            this.labpromgeneral.Text = "Promedio general";
+            this.labpromgeneral.Visible = false;
+            // 
+            // textpromgeneral
+            // 
+            this.textpromgeneral.Location = new System.Drawing.Point(191, 435);
+            this.textpromgeneral.Name = "textpromgeneral";
+            this.textpromgeneral.ReadOnly = true;
+            this.textpromgeneral.Size = new System.Drawing.Size(103, 23);
+            this.textpromgeneral.TabIndex = 19;
+            this.textpromgeneral.Visible = false;
+            // 
+            // Continuar
+            // 
+            this.Continuar.Location = new System.Drawing.Point(119, 483);
+            this.Continuar.Name = "Continuar";
+            this.Continuar.Size = new System.Drawing.Size(101, 32);
+            this.Continuar.TabIndex = 20;
+            this.Continuar.Text = "Continuar";
+            this.Continuar.UseVisualStyleBackColor = true;
+            this.Continuar.Visible = false;
+            this.Continuar.Click += new System.EventHandler(this.Continuar_Click);
+            // 
+            // textcantprom
+            // 
+            this.textcantprom.Location = new System.Drawing.Point(226, 170);
+            this.textcantprom.Name = "textcantprom";
+            this.textcantprom.Size = new System.Drawing.Size(100, 23);
+            this.textcantprom.TabIndex = 21;
+            this.textcantprom.Visible = false;
+            // 
+            // Capitulo6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(465, 527);
+            this.Controls.Add(this.textcantprom);
+            this.Controls.Add(this.Continuar);
+            this.Controls.Add(this.textpromgeneral);
+            this.Controls.Add(this.labpromgeneral);
+            this.Controls.Add(this.textcalifmayor);
+            this.Controls.Add(this.labcalifmayor);
+            this.Controls.Add(this.textcalifmenor);
+            this.Controls.Add(this.labcalifmenor);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.salonsigte);
+            this.Controls.Add(this.labprom);
+            this.Controls.Add(this.listprom);
+            this.Controls.Add(this.Aceptar2);
+            this.Controls.Add(this.textcalifalumnos);
+            this.Controls.Add(this.labcalifalumnos);
+            this.Controls.Add(this.Aceptar1);
+            this.Controls.Add(this.textcantalumnos);
+            this.Controls.Add(this.labcantalumnos);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.textcantsalones);
+            this.Controls.Add(this.labcantsalones);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Capitulo6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Capitulo 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labcantsalones;
+        private System.Windows.Forms.TextBox textcantsalones;
+        private System.Windows.Forms.Button Aceptar;
+        private System.Windows.Forms.Label labcantalumnos;
+        private System.Windows.Forms.TextBox textcantalumnos;
+        private System.Windows.Forms.Button Aceptar1;
+        private System.Windows.Forms.Label labcalifalumnos;
+        private System.Windows.Forms.TextBox textcalifalumnos;
+        private System.Windows.Forms.Button Aceptar2;
+        private System.Windows.Forms.ListBox listprom;
+        private System.Windows.Forms.Label labprom;
+        private System.Windows.Forms.Button salonsigte;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Label labcalifmenor;
+        private System.Windows.Forms.TextBox textcalifmenor;
+        private System.Windows.Forms.Label labcalifmayor;
+        private System.Windows.Forms.TextBox textcalifmayor;
+        private System.Windows.Forms.Label labpromgeneral;
+        private System.Windows.Forms.TextBox textpromgeneral;
+        private System.Windows.Forms.Button Continuar;
+        private System.Windows.Forms.TextBox textcantprom;
+    }
+}
